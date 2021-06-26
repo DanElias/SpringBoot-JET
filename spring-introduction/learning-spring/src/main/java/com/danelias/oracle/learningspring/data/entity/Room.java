42894e87cd7315966ad1eb69c408baf5c4e787a7
Daniel Elias <danielelias@Daniels-MacBook-Pro.local>
2021-06-26 16:04:17 -0500

    Start first course

---
+++ spring-introduction/learning-spring/src/main/java/com/danelias/oracle/learningspring/data/entity/Room.java
@@ -0,0 +1,22 @@
+package com.danelias.oracle.learningspring.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import java.lang.annotation.Repeatable;
+
+/*
+@Entity
+@Table(Name="ROOM")
+public class Room {
+    @Id
+    @Column(name="ROOM_ID")
+    @GeneratedValue(strategy = _GenerationType.AUTO)
+    private long roomId;
+    @Column(name="NAME")
+    private String roomName;
+    @Column(name="ROOM_NUMBER")
+    private String roomNumber;
+    @Column(name="BED_INFO")
+    private String bedInfo;
+}
+*/
---
+++ spring-introduction/learning-spring/src/test/java/com/danelias/oracle/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danelias.oracle.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
