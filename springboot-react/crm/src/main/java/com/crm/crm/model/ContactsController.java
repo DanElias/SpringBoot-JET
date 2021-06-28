d1a9021ce8fa3ea2073d34c2cc7b5b3a20dc9dcf
Daniel Elias <danielelias@Daniels-MacBook-Pro.local>
2021-06-27 19:42:24 -0500

    Finish full-stack app springboot and react

---
+++ springboot-react/crm/src/main/java/com/crm/crm/model/ContactsController.java
@@ -0,0 +1,35 @@
+package com.crm.crm.model;
+
+// allows the cors for the frontend to send a post request to the api
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ContactsController {
+    private ContactRepository contactRepository;
+
+    public ContactsController(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @GetMapping("/contacts")
+    Collection<Contact> contacts() {
+        return (Collection<Contact>) contactRepository.findAll();
+    }
+
+    @PostMapping("/contacts")
+    ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) throws URISyntaxException {
+        Contact result = contactRepository.save(contact);
+        return ResponseEntity.ok().body(result);
+    }
+
+}
---
+++ springboot-react/crm/src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// to create a frontend client do "npx create-react-app client"
+// to start react client go to client and type "npm start"
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+}
